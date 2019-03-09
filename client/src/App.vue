83b8881cc822583bd1306473cfeef3c9dab3fead
Jonathan <jtc7537@gmail.com>
2019-03-09 00:08:25 -0500

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" @keydown='move' tabindex='0'>
+    <Character :location-x='location.character.x' :location-y='location.character.y'></Character>
+  </div>
+</template>
+
+<script>
+import Character from './components/Character'
+
+export default {
+  name: 'app',
+  components: {
+    Character
+  },
+  data: function () {
+    return {
+      location: {
+        character: {
+          x: 10,
+          y: 10
+        }
+      }
+    }
+  },
+  methods: {
+    move: function (e) {
+      const {keyCode} = e
+      if(keyCode == 37 && this.location.character.x > 0) {this.location.character.x -= 2}
+      if(keyCode == 38 && this.location.character.y > 0) {this.location.character.y -= 2}
+      if(keyCode == 39) {this.location.character.x += 2}
+      if(keyCode == 40) {this.location.character.y += 2}
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 400px;
+  height: 400px;
+  padding: 0px;
+  margin: 0px
+}
+</style>
---
+++ client/src/components/Character.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class='character-container' >
+        <div v-bind:style="{top:this.$props.locationY + 'px', left: this.$props.locationX + 'px'}" class='character'></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Character',
+    props: ['location-x', 'location-y'],
+    data: function () {
+        return {
+            x: 0,
+            y: 0
+        }
+    }
+}
+</script>
+
+<style>
+.character {
+    width: 10px;
+    height: 10px;
+    background-color: black;
+    position: relative
+}
+</style>
